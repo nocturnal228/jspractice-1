a03e3c03c6ce1060b7baf98ea1c7d07abab92e2d
vladyslav <vladyslav.tsyhanenco@gmail.com>
2023-01-25 15:27:25 +0200

    second task solved

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 'use strict'
 
+//1
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
 
-console.log(numberOfFilms)
+//2
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+}
