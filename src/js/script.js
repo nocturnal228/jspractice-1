91e4f17b63f4c9e0fd0238036c834a81f2a04c51
vladyslav <vladyslav.tsyhanenco@gmail.com>
2023-01-25 15:45:32 +0200

    third task solved

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,11 @@
 'use strict'
 
 //1
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+	nameOfLastMovie = prompt('Один из последних просмотренных фильмов?', ''),
+	rateOfLastMovie = +prompt('На сколько оцените его?', ''),
+	nameOfLastMovie1 = prompt('Один из последних просмотренных фильмов?', ''),
+	rateOfLastMovie2 = +prompt('На сколько оцените его?', '')
 
 //2
 const personalMovieDB = {
@@ -11,3 +15,8 @@ const personalMovieDB = {
 	genres: [],
 	privat: false
 }
+//
+personalMovieDB.movies[nameOfLastMovie] = rateOfLastMovie
+personalMovieDB.movies[nameOfLastMovie1] = rateOfLastMovie2
+
+console.log(personalMovieDB)
